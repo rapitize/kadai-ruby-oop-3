594480320ed38782a50c0c69dd6415d211fa0c0d
zukABi <dlog8meno_4798msm@yahoo.co.jp>
2021-08-17 13:43:07 +0000

    second commit

--- main.rb
+++ main.rb
@@ -2,8 +2,8 @@ require './human'
 
 # インスタンスの作成
 tanaka = Human.new('田中 太郎', 25, '電車')
-suzuki = Human.new('鈴木 次郎',	30,	'野球')
-sato   = Human.new('佐藤 花子',	20,	'映画')
+suzuki = Human.new('鈴木 次郎',	30, '野球')
+sato   = Human.new('佐藤 花子',	20, '映画')
 
 # メソッドを実行
 tanaka.say
